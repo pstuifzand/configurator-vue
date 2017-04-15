8f1e81d1a41e2f0cbd6d44d8a648d8f246701c16
Peter Stuifzand <peter@stuifzand.eu>
2017-04-15 11:55:46 +0200

    CONF-11 Get choices data from backend

--- src/components/Configurator.vue
+++ src/components/Configurator.vue
@@ -15,6 +15,7 @@
 
 <script>
 import _ from 'lodash'
+import axios from 'axios'
 import Choice from './Choice'
 
 export default {
@@ -24,82 +25,15 @@ export default {
     'config-choice': Choice
   },
 
+  mounted () {
+    axios.get('http://localhost:8002/').then(response => {
+      this.choices = response.data
+    })
+  },
+
   data () {
     return {
-      choices: [
-        {
-          id: 1,
-          title: 'Kies je productiemethode',
-          name: 'production',
-          open: true,
-          selected: false,
-          type: 'radio',
-          options: [
-            { title: 'Digitaal', id: 1 },
-            { title: 'Offset', id: 2 }
-          ]
-        },
-        {
-          id: 2,
-          title: 'Kies het eindformaat',
-          name: 'size',
-          open: false,
-          selected: false,
-          type: 'radio',
-          options: [
-            { title: 'A4', id: 1 },
-            { title: 'A5', id: 2 },
-            { title: 'A6', id: 3 },
-            { title: 'Lang A5', id: 4 },
-            { title: 'Vierkant (210mm x 210mm)', id: 5 },
-            { title: 'Vierkant (135mm x 135mm)', id: 6 }
-          ]
-        },
-        {
-          id: 3,
-          title: 'Kies de vouwrichting',
-          name: 'size',
-          open: false,
-          selected: false,
-          type: 'radio',
-          options: [
-            { title: 'Staand', id: 1 },
-            { title: 'Liggend', id: 1 }
-          ]
-        },
-        {
-          id: 4,
-          title: 'Aantal pagina\'s inclusief omslag',
-          name: 'page_count',
-          open: false,
-          selected: false,
-          type: 'select',
-          options: [
-            { title: '8 Pagina\'s', id: 1 },
-            { title: '12 Pagina\'s', id: 2 },
-            { title: '16 Pagina\'s', id: 3 },
-            { title: '20 Pagina\'s', id: 4 },
-            { title: '24 Pagina\'s', id: 5 },
-            { title: '28 Pagina\'s', id: 6 }
-          ]
-        },
-        {
-          id: 5,
-          title: 'Kies de oplage',
-          name: 'size',
-          open: false,
-          selected: false,
-          type: 'radio',
-          options: [
-            { title: '100', id: 1 },
-            { title: '250', id: 2 },
-            { title: '500', id: 3 },
-            { title: '1000', id: 4 },
-            { title: '2000', id: 5 },
-            { title: '4000', id: 6 }
-          ]
-        }
-      ]
+      choices: []
     }
   },
 
